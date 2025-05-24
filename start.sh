65952a284f5c3c08c52613de5e6a2bf596f5699f
mumtazharis <ahmadmumtazhrs@gmail.com>
2025-05-24 23:20:28 +0700

    a

--- start.sh
+++ start.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+# Pastikan executable bisa dijalankan
 chmod +x /home/ubuntu/tescicd
-/home/ubuntu/tescicd > /dev/null 2>&1 &
+
+# Jalankan aplikasi Go di background dan arahkan output ke file log
+nohup /home/ubuntu/tescicd > /home/ubuntu/tescicd.log 2>&1 &
+
+echo "Aplikasi Go 'tescicd' telah dimulai di background."
